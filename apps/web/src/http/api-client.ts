3ba0ca201e19430b6ac753c808b397f2792ebf7a
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2025-04-28 21:39:12 -0300

    Obtendo usuário autenticado

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,26 @@
+import { getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let token: string | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: getServerCookies } = await import('next/headers')
+
+          const cookieStore = await getServerCookies()
+          token = cookieStore.get('token')?.value
+        } else {
+          token = getCookie('token') as string | undefined
+        }
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
